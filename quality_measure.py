461b7405fafbc2a9cab69149b42d6e4ac20537bf
RianneSchouten <rianneschouten@live.nl>
2021-11-22 20:21:11 +0100

    add qm with no correction

--- quality_measure.py
+++ quality_measure.py
@@ -6,7 +6,7 @@ import data_refinement as refine
 
 # function which routes to the specified quality measure
 def set_quality(description: refine.Description, data: refine.DataSet, method: refine.Method):
-    if method in [refine.Method.OUR_N, refine.Method.OUR_SQRT, refine.Method.OUR_ENTROPY]:
+    if method in [refine.Method.OUR_N, refine.Method.OUR_SQRT, refine.Method.OUR_ENTROPY, refine.Method.OUR_NONE]:
         return our_quality_measure(description, data, method)
     elif method in [refine.Method.NORM, refine.Method.LABELWISE, refine.Method.PAIRWISE]:
         return wouters_quality_measure(description=description, data=data, method=method)
@@ -17,13 +17,15 @@ def set_quality(description: refine.Description, data: refine.DataSet, method: r
 # function which computes our currently defined quality measure
 def our_quality_measure(description: refine.Description, data: refine.DataSet, method: refine.Method):
     subgroup_data = refine.get_subgroup_data(description, data.dataframe)[data.targets]  # get subgroup rows from data_refinement
+    #print(len(subgroup_data))
     complement_data = data.dataframe.drop(labels=subgroup_data.index, axis="rows")[data.targets].to_numpy()  # obtain the complement rows
+    #print(len(complement_data))
     subgroup_data = subgroup_data.to_numpy()
     subgroup_rows = len(subgroup_data)
     complement_rows = len(complement_data)
     all_rows = subgroup_rows + complement_rows
 
-    if subgroup_rows <= 1 or complement_rows == 0:
+    if subgroup_rows <= 35 or complement_rows == 0:
         description.quality = 0.0
         return
 
@@ -34,13 +36,17 @@ def our_quality_measure(description: refine.Description, data: refine.DataSet, m
 
         # faster method
         d_x_x_sum += np.sqrt(np.sum(((sub_target_vector - subgroup_data) ** 2), axis=1)).sum()
-        # d_x_y_sum += np.sqrt(np.sum(((sub_target_vector - complement_data) ** 2), axis=1)).sum()
+        #d_x_y_sum += np.sqrt(np.sum(((sub_target_vector - complement_data) ** 2), axis=1)).sum()
         d_x_y_sum += np.sqrt(
             np.sum(((sub_target_vector - data.rank) ** 2)))  # same as np.linalg.norm(sub_target_vector - data.rank)
 
     numerator = (1.0 / subgroup_rows) * d_x_y_sum
-    # numerator = (1.0 / (subgroup_rows * complement_rows)) * d_x_y_sum
+    #numerator = (1.0 / (subgroup_rows * complement_rows)) * d_x_y_sum
     denominator = (1.0 / (subgroup_rows * (subgroup_rows - 1))) * d_x_x_sum
+
+    #print(numerator)
+    #print(denominator)
+    #print(numerator/denominator)
 
     if method == refine.Method.OUR_N:
         description.quality = (numerator / (denominator + 1)) * subgroup_rows
@@ -50,7 +56,10 @@ def our_quality_measure(description: refine.Description, data: refine.DataSet, m
         description.quality = (numerator / (denominator + 1)) * (
                     -((subgroup_rows / all_rows) * log10(subgroup_rows / all_rows)) - (
                         (complement_rows / all_rows) * log10(complement_rows / all_rows)))
+    elif method == refine.Method.OUR_NONE:
+        description.quality = (numerator / (denominator + 1))
 
+    #print(description.quality)
 
 # compute distance between input vector and matrix given the specified distance function
 def compute_distance(vector: [int], matrix, function: str):
